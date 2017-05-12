3bb07442583cba9e49a79581da966d718328984e
Luc Flynn <lucnmsu@gmail.com>
2017-05-12 11:43:03 -0600

    finished build--bacon works

--- public_html/api/fuzzy/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-$fuzzy = ["meow", "feed the kitty", "love the kitty"];
-echo json_encode($fuzzy);
--- webpack/index.php
+++ webpack/index.php
@@ -1,12 +1,17 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <base href="/" />
+    <base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
 
-    <title>Twitter Data Design Example</title>
+    <title>Angular 4 Spinup</title>
 </head>
 <body>
 <angular4spin>Loading&hellip;</angular4spin>
